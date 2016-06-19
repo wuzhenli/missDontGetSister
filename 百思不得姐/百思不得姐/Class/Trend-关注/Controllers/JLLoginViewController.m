e28b38e98fc3bb1f4b6d487ab2c204e008aeb408
wuzhenli <wuzhenli@kongfz.com>
2016-06-19 17:45:18 +0800

    1、登录界面、输入框、注册框动画

--- 百思不得姐/百思不得姐/Class/Trend-关注/Controllers/JLLoginViewController.m
+++ 百思不得姐/百思不得姐/Class/Trend-关注/Controllers/JLLoginViewController.m
@@ -10,11 +10,17 @@
 
 @interface JLLoginViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UIButton *regiserButton;
 
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 
 
 @property (weak, nonatomic) IBOutlet UITextField *pwdField;
+
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *registerCenterX;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginCenterX;
+
 @end
 
 @implementation JLLoginViewController
@@ -34,10 +40,34 @@
 - (void)setUp {
     self.loginButton.layer.cornerRadius = 5;
     self.loginButton.clipsToBounds = YES;
+    self.regiserButton.layer.cornerRadius = 5;
+    self.regiserButton.clipsToBounds = YES;
+    //
+
 }
 
 - (IBAction)goBack:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)loginOrRegister:(UIButton *)sender {
+    NSString *title = sender.currentTitle;
+    CGFloat loginCenterX = 0.;
+    CGFloat registerCenterX = 0.;
+    if ( [title isEqualToString:@"注册"] ) {
+        [sender setTitle:@"登录" forState:UIControlStateNormal];
+        loginCenterX = -SC_Width;
+        registerCenterX = 0;
+    }else {
+        [sender setTitle:@"注册" forState:UIControlStateNormal];
+        loginCenterX = 0;
+        registerCenterX = SC_Width;
+    }
+    self.loginCenterX.constant = loginCenterX;
+    self.registerCenterX.constant = registerCenterX;
+    [UIView animateWithDuration:0.25 animations:^{
+            [self.view layoutIfNeeded];
+    }];
+    
 }
 
 @end
